c9ac988d52574232cd13e70ef1d800fac4f24f39
DaianaChile <daiarocio@outlook.com>
2022-12-24 04:35:55 -0300

    first commit

---
+++ src/main/java/com/portfolio/mgb/DaiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaiApplication.class, args);
+	}
+
+}
